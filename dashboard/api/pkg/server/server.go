4071c223bb0c14a877e3e058ca908e88d17abcd8
Luke Marsden <me@lukemarsden.net>
2023-01-24 15:21:59 +0000

    include dashboard changes from dashboard-stable-diffusion-endpoint branch

--- dashboard/api/pkg/server/server.go
+++ dashboard/api/pkg/server/server.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -50,6 +51,7 @@ func (apiServer *DashboardAPIServer) ListenAndServe(ctx context.Context, cm *sys
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 	subrouter.HandleFunc("/nodes", apiServer.nodes).Methods("GET")
+	subrouter.HandleFunc("/stablediffusion", apiServer.stablediffusion).Methods("POST")
 	subrouter.HandleFunc("/jobs", apiServer.jobs).Methods("POST")
 	subrouter.HandleFunc("/jobs/count", apiServer.jobsCount).Methods("POST")
 	subrouter.HandleFunc("/job/{id}", apiServer.job).Methods("GET")
@@ -68,13 +70,46 @@ func (apiServer *DashboardAPIServer) ListenAndServe(ctx context.Context, cm *sys
 
 	srv := &http.Server{
 		Addr:              fmt.Sprintf("%s:%d", apiServer.Options.Host, apiServer.Options.Port),
-		WriteTimeout:      time.Second * 15,
-		ReadTimeout:       time.Second * 15,
-		ReadHeaderTimeout: time.Second * 15,
-		IdleTimeout:       time.Second * 60,
+		WriteTimeout:      time.Minute * 15,
+		ReadTimeout:       time.Minute * 15,
+		ReadHeaderTimeout: time.Minute * 15,
+		IdleTimeout:       time.Minute * 60,
 		Handler:           router,
 	}
 	return srv.ListenAndServe()
+}
+
+type PromptParam struct {
+	Prompt string `json:"prompt"`
+}
+
+func (apiServer *DashboardAPIServer) stablediffusion(res http.ResponseWriter, req *http.Request) {
+	// any crazy mofo on the planet can build this into their web apps
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// TODO: read "prompt" key of POST'ed JSON, or error
+	promptParam := PromptParam{}
+	err := json.NewDecoder(req.Body).Decode(&promptParam)
+	if err != nil {
+		_, _ = res.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, strings.Trim(err.Error(), "\n"))))
+		return
+	}
+	prompt := promptParam.Prompt
+
+	// user can pass ?testing=1 to bypass GPU and just return the prompt
+	testing := len(req.URL.Query()["testing"]) > 0
+
+	log.Info().Msgf("--> testing=%t", testing)
+
+	// TODO: put client code in here, hoisted directly from CLI
+	cid, err := runStableDiffusion(prompt, testing)
+	if err != nil {
+		log.Error().Err(err).Send()
+		_, _ = res.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, strings.Trim(err.Error(), "\n"))))
+	} else {
+		log.Info().Str("CID", cid).Send()
+		_, _ = res.Write([]byte(fmt.Sprintf(`{"cid": "%s"}`, strings.Trim(cid, "\n"))))
+	}
 }
 
 func (apiServer *DashboardAPIServer) annotations(res http.ResponseWriter, req *http.Request) {
