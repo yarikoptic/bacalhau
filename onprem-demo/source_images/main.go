a2896e22dd0927261f89ee5762c409bcae394545
Luke Marsden <me@lukemarsden.net>
2023-01-27 15:04:57 +0000

    source_images

--- onprem-demo/source_images/main.go
+++ onprem-demo/source_images/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -16,6 +17,14 @@ func main() {
 	}
 	defer watcher.Close()
 
+	imageSinkDir := os.Getenv("ONPREM_DEMO_IMAGE_SINK_DIR")
+	if imageSinkDir == "" {
+		msg := "Please specify ONPREM_DEMO_IMAGE_SINK_DIR as an absolute path to where " +
+			"you have configured your Bacalhau job's output directory, e.g. /output"
+		fmt.Println(msg)
+		os.Exit(1)
+
+	}
 	// Start listening for events.
 	go func() {
 		for {
@@ -27,7 +36,14 @@ func main() {
 				log.Println("event:", event)
 				if event.Has(fsnotify.Write) {
 					log.Println("modified file:", event.Name)
+					// XXX what if the whole file write isn't finished yet
+					err, _ := copy(event.Name, imageSinkDir+"/"+"image.jpg")
+					if err != nil {
+						log.Printf("error copying file: %s", err)
+					}
+					// TODO: http request
 				}
+
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
@@ -53,4 +69,29 @@ func main() {
 
 	// Block main goroutine forever.
 	<-make(chan struct{})
+}
+
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
