7902a5e485b998689949ffbda8f78f7f145bf1f7
Luke Marsden <me@lukemarsden.net>
2023-02-02 16:14:14 +0000

    rebuild script

---
+++ onprem-demo/scripts/rebuild.sh
@@ -0,0 +1,4 @@
+CGO_ENABLED=0 go install
+scp /home/luke/gocode/bin/bacalhau luke@mind.local:gocode/bin/bacalhau
+scp /home/luke/gocode/bin/bacalhau newluke@hostess.local:gocode/bin/bacalhau
+scp /home/luke/gocode/bin/bacalhau luke@light.local:gocode/bin/bacalhau
