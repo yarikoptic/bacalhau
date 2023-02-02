68f2b5c68ac01e30759654a8d1dff25a003b02c8
Luke Marsden <me@lukemarsden.net>
2023-02-02 17:30:03 +0000

    Add node selectors on jobs

--- onprem-demo/scripts/start.sh
+++ onprem-demo/scripts/start.sh
@@ -1,3 +1,4 @@
 bacalhau create --id-only --wait=false /tmp/onprem-demo-job-inference-server.yaml
 bacalhau create --id-only --wait=false /tmp/onprem-demo-job-logs.yaml
 bacalhau create --id-only --wait=false /tmp/onprem-demo-job-images.yaml
+
