d09024222a9bb8543d1cd8a8e05d58f571d316d8
Ross Jones <ross@register-dynamics.co.uk>
2023-03-13 13:39:07 +0000

    Adds the compute node end of the logging pipeline
    Adds a new endpoint in the compute node to allow a caller to request a
    multiaddr where the logserver is listening for new connections (using a
    protocol ID of its own). Logging was not added to bprotocol directly as
    it is a request/response protocol, whereas logging is a streaming,
    long-lived response.

--- pkg/node/compute.go
+++ pkg/node/compute.go
@@ -8,6 +8,7 @@ import (
 	compute_bidstrategies "github.com/bacalhau-project/bacalhau/pkg/compute/bidstrategy"
 	"github.com/bacalhau-project/bacalhau/pkg/compute/capacity"
 	"github.com/bacalhau-project/bacalhau/pkg/compute/capacity/disk"
+	"github.com/bacalhau-project/bacalhau/pkg/compute/logstream"
 	compute_publicapi "github.com/bacalhau-project/bacalhau/pkg/compute/publicapi"
 	"github.com/bacalhau-project/bacalhau/pkg/compute/sensors"
 	"github.com/bacalhau-project/bacalhau/pkg/compute/store"
@@ -34,6 +35,7 @@ type Compute struct {
 	Capacity            capacity.Tracker
 	ExecutionStore      store.ExecutionStore
 	Executors           executor.ExecutorProvider
+	LogServer           *logstream.LogStreamServer
 	computeCallback     *bprotocol.CallbackProxy
 	cleanupFunc         func(ctx context.Context)
 	computeInfoProvider model.ComputeNodeInfoProvider
@@ -167,6 +169,14 @@ func NewComputeNode(
 		}),
 	)
 
+	// logging server
+	logserver := logstream.NewLogStreamServer(ctx, host, executionStore, executors)
+	_, loggingCancel := context.WithCancel(ctx)
+	cleanupManager.RegisterCallback(func() error {
+		loggingCancel()
+		return nil
+	})
+
 	// node info
 	nodeInfoProvider := compute.NewNodeInfoProvider(compute.NodeInfoProviderParams{
 		Executors:          executors,
@@ -181,6 +191,7 @@ func NewComputeNode(
 		UsageCalculator: capacityCalculator,
 		BidStrategy:     biddingStrategy,
 		Executor:        bufferRunner,
+		LogServer:       *logserver,
 	})
 
 	// if this node is the simulator, then we set the simulator request handler as the stream handler
@@ -222,6 +233,7 @@ func NewComputeNode(
 		Capacity:            runningCapacityTracker,
 		ExecutionStore:      executionStore,
 		Executors:           executors,
+		LogServer:           logserver,
 		computeCallback:     standardComputeCallback,
 		cleanupFunc:         cleanupFunc,
 		computeInfoProvider: nodeInfoProvider,
